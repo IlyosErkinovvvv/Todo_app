2fa2a3bf9f0b6eec5758278573ba364f4ac6d872
IlyosErkinovvvv <ilyoserkinovvv@gmail.com>
2023-06-15 19:03:08 +0500

    Ilyos added application

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rolling_switch/rolling_switch.dart';
 import 'package:todo_app/data/model/todo_model.dart';
